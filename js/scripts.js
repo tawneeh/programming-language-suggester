e268ac32fc7ff526ec71174f9ad06adf8e871cca
Tawnee Harris <tawneeh@icloud.com>
2020-10-23 12:43:44 -0700

    adjust html formatting. change questions from yes or no to two different choices

--- js/scripts.js
+++ js/scripts.js
@@ -2,14 +2,10 @@ $(document).ready(function() {
   $("#questions").submit(function(event) {
     event.preventDeafult();
 
-    
-    let dog = $("input[name="dog"]:checked").val(true);
-      if (dog === true) {
-        $('#javascript').show();
-      } else if (dog === false) {
-        $('#c-sharp').show();
-      } else if (music === true) {
-        $('#ruby').show();
-      }
+    let 
+
+
+
+
   });
 });
