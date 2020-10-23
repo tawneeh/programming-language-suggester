4662048661f5de43a6b8cc9335a30676b10ba46b
Tawnee Harris <tawneeh@icloud.com>
2020-10-23 13:22:34 -0700

    add changes to the function

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,27 @@
+
+
+
 $(document).ready(function() {
   $("#questions").submit(function(event) {
-    event.preventDeafult();
+    event.preventDefault();
 
-    let 
+    let dog = $("input:radio[name=dog]:checked").val();
 
+    let color = $("input:radio[name=color]:checked").val();
 
+    let hike = $("input:radio[name=beverage]:checked").val();
 
+    let music = $("input:radio[name=music]:checked").val();
+
+    let total = totalValue (dog, color, hike, beverage, music);
+
+    $("#javascript").hide();
+    $("#ruby").hide();
+    $("#csharp").hide();
+
+    if (total>= 5) {
+      $('#javascript').show();
+    }
 
   });
 });
