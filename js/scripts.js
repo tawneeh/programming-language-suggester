b4a2fa044a07e32e3a9e9f4a0173e33e743df2d6
Austin <aokincaid@gmail.com>
2020-10-28 21:29:18 -0700

    Add some alternate forms to perform the functions already made, include lots of notes

--- js/scripts.js
+++ js/scripts.js
@@ -2,29 +2,55 @@ function totalValue (num1, num2, num3, num4, num5) {
   return parseInt(num1) + parseInt(num2) + parseInt(num3) + parseInt(num4) + parseInt(num5)
 }
 
+function totalValueWithArgs() {
+  // Arguments is a convience thing, apparently you can pass any number of
+  // inputs to any method and access them through arguments. It's an array.
+  let total = 0
+  for (const value in arguments) {
+    total += parseInt(value)
+  }
+  return total
+
+  // The following line is the oneline version of the above using a functional
+  // style. It's my fave style but takes some time to understand. I promise it's
+  // the best :) This might not work like the other one, I didn't test this one.
+  // return arguments.reduce((total, arg) => total + parseInt(arg), 0)
+}
+
+function getInputValue(input) {
+  // In the following line I use string interpolation to insert the input into
+  // the string that's executed by jQuery. The format is with backtics some
+  // characters, and something ou want inserted (that is a string), like this:
+  // `Abolish ${someValue}` where someValue = ICE :)
+  const stringValue = $(`input:radio[name='${input}']:checked`).val()
+  // May as well parse the Int here, why not!
+  return parseInt(stringValue)
+}
+
+const inputs = ['dog', 'color', 'hike', 'beverage', 'music']
+
 $(document).ready(function() {
   $("#questions").submit(function(event) {
 
-    let dog = $("input:radio[name='dog']:checked").val();
+    let total = 0
+    inputs.forEach(function (input) { total += getInputValue(input) })
 
-    let color = $("input:radio[name='color']:checked").val();
+    // Another Functional example. Reduce is one of the trickier Functional
+    // methods. It needs a function that returns a value and a starting value.
+    // in this case, the 0 is the starting value, which is passed in during
+    // the first call of the method as total. What's returned is used in the
+    // next calling as total.
+    // const total = inputs.reduce(function (total, arg) { return total + getInputValue(arg) }, 0)
 
-    let hike = $("input:radio[name='hike']:checked").val();
-
-    let beverage = $("input:radio[name='beverage']:checked").val();
-
-    let music = $("input:radio[name='music']:checked").val();
-
-    let total = totalValue (dog, color, hike, beverage, music);
-
+    // I added the "="s to the > and < to cover those cases where total = 5 and
+    // 8. It happened to me while testing, haha.
     if (total === 5) {
       $("#javascript").show();
-    } else if (total > 8) {
+    } else if (total >= 8) {
       $("#ruby").show();
-    } else if (total < 5) {
+    } else if (total <= 5) {
       $("#csharp").show();
     }
   event.preventDefault();
-
   });
 });
